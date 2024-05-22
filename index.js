4e29f819c43804d2eb628d47b16954c5c2bf8401
Jonathan Cantor <jonatcantor@gmail.com>
2024-05-22 16:44:55 -0400

    Add backend

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const plantaSolarRoutes = require('./routes/plantaSolarRoutes');
+const path = require('path'); // Importa el módulo 'path'
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+// Usar las rutas de la planta solar
+app.use('/planta_solar', plantaSolarRoutes);
+
+// Puerto del servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor Express en ejecución en el puerto ${PORT}`);
+});
+
+// Middleware para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Exportar el manejador para Vercel
+module.exports = app;
---
+++ routes/plantaSolarRoutes.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const router = express.Router();
+
+// Inicializa Firebase Admin SDK (asegúrate de que el archivo JSON de tu clave de servicio esté correctamente configurado)
+const serviceAccount = require("../paneles-solares-a778d-firebase-adminsdk-jkgy3-5da918275d.json");
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL.replace(/\\n/g, '\n'),
+});
+
+// Rutas para la planta solar
+
+// Obtener todos los datos de la planta solar
+router.get('/', async (req, res) => {
+  try {
+    const db = admin.database();
+    const ref = db.ref('planta_solar');
+    const snapshot = await ref.once('value');
+    const data = snapshot.val();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Crear una nueva planta solar
+router.post('/', async (req, res) => {
+  try {
+    const db = admin.database();
+    const ref = db.ref('planta_solar/lecturas');
+    const newPlantaSolarRef = ref.push();
+    await newPlantaSolarRef.set(req.body);
+    res.status(201).json({ message: 'Planta solar creada exitosamente' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Actualizar una planta solar existente
+router.put('/:lecturaId', async (req, res) => {
+  try {
+    const { lecturaId } = req.params;
+    const db = admin.database();
+    const ref = db.ref(`planta_solar/lecturas/${lecturaId}`);
+    await ref.update(req.body);
+    res.json({ message: 'Planta solar actualizada exitosamente' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Eliminar una planta solar
+router.delete('/:lecturaId', async (req, res) => {
+  try {
+    const { lecturaId } = req.params;
+    const db = admin.database();
+    const ref = db.ref(`planta_solar/lecturas/${lecturaId}`);
+    await ref.remove();
+    res.json({ message: 'Planta solar eliminada exitosamente' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
